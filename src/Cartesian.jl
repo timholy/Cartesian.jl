288d4d533637134b97ae7045466ddb9ed24f4e16
timholy <tim.holy@gmail.com>
2013-07-23 09:25:30 -0500

    Add fast SubArray operations

--- src/Cartesian.jl
+++ src/Cartesian.jl
@@ -1,6 +1,6 @@
 module Cartesian
 
-export @forcartesian
+export @forcartesian, @forarrays, parent
 
 macro forcartesian(sym, sz, ex)
     idim = gensym()
@@ -26,6 +26,118 @@ macro forcartesian(sym, sz, ex)
                 end
             end
         end
+    end
+end
+
+### Fast SubArray (and Array) operations ###
+parent(A::Array) = A
+parent(S::SubArray) = S.parent
+index(A::Array, dim::Integer, i::Integer) = i
+index(S::SubArray, dim::Integer, i::Integer) = S.indexes[dim][i]
+# Calculate the offset due to trailing/sliced singleton dimensions
+sliceoffset(A::Array) = 0
+function sliceoffset(S::SubArray)
+    missingdims = setdiff(1:ndims(parent(S)), Base.parentdims(S))
+    off = 0
+    for i in missingdims
+        off += (S.indexes[i][1]-1)*stride(parent(S), i)
+    end
+    off
+end
+
+namedvar(base::Symbol, ext) = symbol(string(base)*string(ext))
+namedvar(base::Symbol, ext1, ext2) = symbol(string(base)*string(ext1)*string(ext2))
+
+# Generate expressions like :( sliceA = sliceoffset(A) )
+function sliceoffsetexpr(array::Symbol)
+    slice = namedvar(:slice, array)
+    return :($(esc(slice)) = sliceoffset($(esc(array))))
+end
+
+# Generate expressions like :( o3 = :slice3 + (i3-1)*stride3 ), using strides appropriate for a particular array
+function offsetexpr(offset::Symbol, iter::Symbol, array::Symbol, dim::Integer)
+    ocur = dim == 1 ? namedvar(offset, array) : namedvar(offset, array, dim)
+    icur = namedvar(iter, dim)
+    scur = namedvar(:stride, array, dim)
+    slice = namedvar(:slice, array)
+    return :($(esc(ocur)) = $(esc(slice)) + (index($(esc(array)), $dim, $(esc(icur)))-1)*$(esc(scur)))
+end
+
+# Generate expressions like :( o2 = o3 + (i2-1)*stride2 ), using strides appropriate for a particular array
+function nestedoffsetexpr(offset::Symbol, iter::Symbol, array::Symbol, dim::Integer)
+    ocur = dim == 1 ? namedvar(offset, array) : namedvar(offset, array, dim)
+    oprev = namedvar(offset, array, dim+1)
+    icur = namedvar(iter, dim)
+    scur = namedvar(:stride, array, dim)
+    if dim == 1
+        return :($(esc(ocur)) = $(esc(oprev)) + index($(esc(array)), $dim, $(esc(icur))))
+    else
+        return :($(esc(ocur)) = $(esc(oprev)) + (index($(esc(array)), $dim, $(esc(icur)))-1)*$(esc(scur)))
+    end
+end
+
+# Generate expressions like :( strideA3 = strideA2 * size(parent(A), 3) )
+# function stride1expr(array::Symbol)
+#     s = namedvar(:stride, array, 1)
+#     return :($(esc(s)) = stride($(esc(array)), 1))
+# end
+
+function strideexpr(array::Symbol, dim::Integer)
+    s = namedvar(:stride, array, dim)
+    return :($(esc(s)) = stride($(esc(array)), $dim))
+#     sprev = namedvar(:stride, array, dim-1)
+#     return :($(esc(scur)) = $(esc(sprev))*size(parent($(esc(array))), $(dim-1)))
+end
+
+excat(exlist) = length(exlist) == 1 ? exlist[1] : Expr(:block, exlist...)
+
+# Note in args, the first n-1 are array symbols, the last is the expression
+macro forarrays(N, offsetsym, itersym, args...)
+    if !isa(N, Integer)
+        error("First argument must be the number of dimensions (as an integer)")
+    end
+    if !isa(offsetsym, Symbol)
+        error("Second argument must be the base-name of the offset variable")
+    end
+    if !isa(itersym, Symbol)
+        error("Third argument must be the base-name of the coordinate (iteration) variable")
+    end
+    if length(args) < 2
+        error("Supply at least one array and the inner-loop expression")
+    end
+    if !isa(args[end], Expr)
+        error("The final argument must be the inner-loop expression")
+    end
+    asyms = args[1:end-1]
+    ex = Expr(:escape, args[end])
+    # Generate N-1 loops, starting with the inner one
+    for i = 1:N-1
+        offsetvars = [nestedoffsetexpr(offsetsym, itersym, asym, i) for asym in asyms]
+        itervar = namedvar(itersym, i)
+        ex = quote
+            for $(esc(itervar)) = 1:size($(esc(asyms[1])), $i)
+                $(excat(offsetvars))
+                $ex
+            end
+        end
+    end
+    # Generate the outer loop, which cannot depend on previous loops (it's not nested)
+    offsetvars = [offsetexpr(offsetsym, itersym, asym, N) for asym in asyms]
+    itervar = namedvar(itersym, N)
+    ex = quote
+        for $(esc(itervar)) = 1:size($(esc(asyms[1])), $N)
+            $(excat(offsetvars))
+            $ex
+        end
+    end
+    # Generate the stride variables and sliceoffset variables
+    headervars = [sliceoffsetexpr(asym) for asym in asyms]
+    for i = 1:N
+        append!(headervars, Expr[strideexpr(asym, i) for asym in asyms])
+    end
+    return quote
+        $(excat(headervars))
+        $ex
     end
 end
 
--- test/tests.jl
+++ test/tests.jl
@@ -15,27 +15,27 @@ function testcounter(m, n, N)
         end
     end
     sz = [m,n]
-    scounter = 0
-    @time begin
-        for k = 1:N
-            c = ones(Int, 2)
-            sz1 = sz[1]
-            isdone = false
-            while !isdone
-                scounter += 1
-                if (c[1]+=1) > sz1
-                    idim = 1
-                    while c[idim] > sz[idim] && idim < 2
-                        c[idim] = 1
-                        idim += 1
-                        c[idim] += 1
-                    end
-                    isdone = c[end] > sz[end]
-                end
-            end
-        end
-    end
-    @assert scounter == sloop
+#     scounter = 0
+#     @time begin
+#         for k = 1:N
+#             c = ones(Int, 2)
+#             sz1 = sz[1]
+#             isdone = false
+#             while !isdone
+#                 scounter += 1
+#                 if (c[1]+=1) > sz1
+#                     idim = 1
+#                     while c[idim] > sz[idim] && idim < 2
+#                         c[idim] = 1
+#                         idim += 1
+#                         c[idim] += 1
+#                     end
+#                     isdone = c[end] > sz[end]
+#                 end
+#             end
+#         end
+#     end
+#     @assert scounter == sloop
     scounter = 0
     @time begin
         for k = 1:N
@@ -45,19 +45,87 @@ function testcounter(m, n, N)
         end
     end
     @assert scounter == sloop
-    scounter = 0
-    sz1 = sz[1]
-    sz[1] = 1
-    @time begin
-        for k = 1:N
-            @Cartesian.forcartesian c sz begin
-                for i = 1:sz1
-                    scounter += 1
-                end
-            end
-        end
-    end
+#     scounter = 0
+#     sz1 = sz[1]
+#     sz[1] = 1
+#     @time begin
+#         for k = 1:N
+#             @Cartesian.forcartesian c sz begin
+#                 for i = 1:sz1
+#                     scounter += 1
+#                 end
+#             end
+#         end
+#     end
 end
 
 testcounter(2,2,1)
 testcounter(500,300,100)
+
+# Specializations for low dimensions
+for N = 1:4
+    @eval begin
+        function mysum{T}(A::StridedArray{T,$N})
+            s = zero(T)
+            Cartesian.@forarrays $N o i A begin
+                s += Cartesian.parent(A)[oA]
+            end
+            s
+        end
+    end
+end
+
+# Function for arbitrary dimensions
+let _mysum_defined = Dict{Any, Bool}()
+global mysum
+function mysum{T,N}(A::StridedArray{T,N})
+    def = get(_mysum_defined, typeof(A), false)
+    if !def
+        ex = quote
+            function _mysum{T}(A::StridedArray{T,$N})
+                s = zero(T)
+                Cartesian.@forarrays $N o i A begin
+                    s += Cartesian.parent(A)[oA]
+                end
+                s
+            end
+        end
+        eval(current_module(), ex)
+        _mysum_defined[N] = true
+    end
+    _mysum(A)
+end
+end
+
+A = reshape(1:120, 3, 5, 8)
+@assert mysum(A) == sum(A)
+subA = sub(A, 1:3, 2:4, 3)
+@assert mysum(subA) == sum(subA)
+
+function mysum2{T}(A::StridedArray{T,2}, B::StridedArray{T,2})
+    if size(A) != size(B)
+        error("dimension mismatch")
+    end
+    ret = zeros(T, size(A))
+    Cartesian.@forarrays 2 o i ret A B begin
+        ret[oret] = Cartesian.parent(A)[oA] + Cartesian.parent(B)[oB]
+    end
+    ret
+end
+
+B = reshape(1:400, 20, 20)
+subB = sub(B, 8:10, 13:15)
+@assert mysum2(subA, subB) == subA+subB
+
+function mydot{T}(A::StridedArray{T,2}, B::StridedArray{T,2})
+    if size(A) != size(B)
+        error("dimension mismatch")
+    end
+    s = zero(T)
+    Cartesian.@forarrays 2 o i A B begin
+        s += Cartesian.parent(A)[oA] * Cartesian.parent(B)[oB]
+    end
+    s
+end
+
+@assert mydot(subA, subB) == sum(subA.*subB) # rhs uses broadcast, which is already fast
